b412fb4494de5f386bfb2ff36fd61c1083e0b41f
Jashan_workspace <Jashan@aws_workspace>
2023-05-22 16:07:36 -0700

    Week16 Exercise

--- exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
+++ exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
@@ -1,7 +1,14 @@
 import pytest
 
 def fizzbuzz(N):
-    pass
+    if N%5 == 0 and N%3 == 0:
+        return "FizzBuzz"
+    elif N%3 == 0:
+        return "Fizz"
+    elif N%5 == 0:
+        return "Buzz"
+    else: 
+        return str(N)
 
 def test_fizzbuzz():
     pass
